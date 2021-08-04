e097f1ccf045b213e4fbc3894c5f3275adce1005
Kennedy Ngure <kngure@fintech-group.com>
2021-08-04 16:41:32 +0300

    logging apis using morgan

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const faker = require('faker');
+const morgan = require('morgan')
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get('/',(req, res) =>{
-    res.send('Hello Api')
+//Middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+require('dotenv/config');
+
+const app_url = process.env.APP_URL;
+const app_port = process.env.APP_PORT;
+const app_prefix = process.env.APP_PREFIX;
+
+
+app.get(`${app_prefix}/product`, (req, res) => {
+    const product = {
+        id: faker.datatype.number(),
+        name: faker.name.findName(),
+        image: faker.image.imageUrl(),
+        address: faker.address.streetAddress() ,
+        employer: faker.company.companyName()
+
+    }
+    res.send(product)
 })
-app.listen(3000,()=>{console.log('Server is running http://localhost:3000')})
+
+app.post(`${app_prefix}/product`, (req, res) => {
+    const newProduct = req.body;
+
+    console.log(newProduct);
+    res.send(newProduct);
+})
+app.listen(app_port, () => {
+    console.log('Server is running ' + app_url + ':' + app_port + app_prefix)
+
+})
+
+
+
