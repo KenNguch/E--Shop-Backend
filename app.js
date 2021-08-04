285976bb8d9570b7f42fbaf7cbca69abba8dcb46
Kennedy Ngure <kngure@fintech-group.com>
2021-08-04 12:16:10 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) =>{
+    res.send('Hello Api')
+})
+app.listen(3000,()=>{console.log('Server is running http://localhost:3000')})
